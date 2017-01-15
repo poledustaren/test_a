519920b88bd7c2b732388d9a3e6351f48b71bbff
Unknown <poledustaren@gmail.com>
2017-01-16 02:53:28 +0500

    home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def index
     @entity=Instrument.new
+    build_page
+  end
+
+  def build_page
     if params[:names].nil?
-      @view_entity = @entity.entities.first
+      root_model
     else
       @entity.params_from_get = params[:names].split('/')
       case @entity.params_from_get.last
@@ -11,28 +15,37 @@ class HomeController < ApplicationController
         when 'edit'
           edit_model
         else
-          @view_entity = @entity.find_entity
+          view_model
       end
     end
   end
 
+  def root_model
+    @view_entity = @entity.entities.first
+  end
+
   def add_model
-    if (params[:entity].nil?)
+    if params[:entity].nil?
       @view_entity = @entity.build_add_entity
       render "home/add"
     else
-      url_path = @entity.add_entity(params[:entity])
+      url_path = @entity.add_entity( params[:entity] )
       redirect_to "#{root_url}#{url_path}"
     end
   end
 
   def edit_model
-    if (params[:entity].nil?)
+    if params[:entity].nil?
       @view_entity = @entity.find_entity
       render "home/edit"
     else
-      url_path=@entity.edit_entity(params[:entity])
+      url_path = @entity.edit_entity(params[:entity])
       redirect_to "#{root_url}#{url_path}"
     end
   end
+
+  def view_model
+    @view_entity = @entity.find_entity
+  end
+
 end
