38476465fc1ede8954ebc15ee58f6176bfffd6eb
Unknown <poledustaren@gmail.com>
2017-01-16 03:02:43 +0500

    required fields

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,11 @@
 class Entity < ActiveRecord::Base
   belongs_to :entity
   has_many :entity
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :title, presence: true
 end
 
-require_relative 'instrument.rb'
+
 
